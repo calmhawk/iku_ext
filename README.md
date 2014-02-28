93f6df8d20d1ef20547cc0d254be1b88e7d940b7
calmhawk <calmhawk@126.com>
2014-02-28 11:12:04 +0800

    add support for youku main page and sub main page

--- README.md
+++ README.md
@@ -11,5 +11,6 @@ please install youkuclient first.
 
 update history
 ---------------------
+* 2014-02-28 support youku main page and sub-main page.
 * 2014-02-27 support weibo text block.
 * 2014-02-26 support v.youku.com, weibo spec block.
--- src/scripts.js
+++ src/scripts.js
@@ -19,18 +19,20 @@
         // fn_favorite is a symbol of swfiKuAdapter's existence we used to start up youkuclient
         // TODO add youku.com inject
         if($('#fn_favorite').length > 0){
-            injectYoukuJs();
+            injectVPageJs();
+        }else{
+            injectMainPageJs();
         }
     }
 
-    function injectYoukuJs(){
+    function injectVPageJs(){
         var m = href.match(/.*id_(.*)\.html.*/);
         if(m && m.length > 1){
             // add a play button beside the favorite button
             /*jshint multistr: true */
             var inject_html = ' \
                 <div class="fn" id="fn_play_iku"> \
-                    <div class="handle" ><a id="fn_play_iku_a" href="' + _genInjectUrl(m[1]) + '">播放</a></div> \
+                    <div class="handle" ><a id="fn_play_iku_a" href="' + _genInjectUrl(m[1]) + '">iKu</a></div> \
                 </div> \
             ';
             $('#fn_favorite').after(inject_html);
@@ -40,6 +42,39 @@
             var inject_js = '<script type="text/javascript"> (function(){var swfiKuAdapter=null;function getIkuAdapter(){swfiKuAdapter=document.getElementById("ikuadapter_swf");return swfiKuAdapter};function ikuExecute(a){return null!=(iku=getIkuAdapter())&&iku.execute?iku.execute(a):"f"};var ele=document.getElementById("fn_play_iku_a");ele.onclick=function(){var a="' + _genInjectUrl(m[1]) + '|";var b="";b=ikuExecute(a);if ("f" == b) return true;else return false;}})(); </script> ';
 
             $('body').append(inject_js); 
+        }
+    }
+
+    function injectMainPageJs(){
+        var m = href.match(/http:\/\/.*\.youku.com\//);
+        if(m && m.length > 0){
+            $('.v-meta.vb').each(function(i, d){
+                $(d).find('.btn.btn-small').each(function(ii, dd){
+                    var mm = dd.href.match(/.*id_(.*)\.html.*/);
+                    if(mm && mm.length > 1){
+                        var inject_html = '<a href="' + _genInjectUrl(mm[1]) + '" style="margin-left:8px" class="btn btn-small">iKu</a>';
+                        $(dd).after(inject_html);
+                    }
+                });
+            });
+            $('.v-meta.va').each(function(i, d){
+                var a = $(d).parent().find('.v-link').find('a');
+                var ele = $(d).find('.ico-statplay');
+                if(a.length > 0 && ele.length > 0){
+                    var mm = a[0].href.match(/.*id_(.*)\.html.*/);
+                    if(mm && mm.length > 1){
+                        var inject_html = '<a href="' + _genInjectUrl(mm[1]) + '" class="v-username"><i class="ico-statplay"></i>iKu</a>';
+                        var TOTAL_DIV_WIDTH = 180; // 190 - 5 * 2(padding)
+                        var INJECT_ELE_WIDTH = 34; 
+                        var existing_ele_width = 0;
+                        $(ele[0]).parent().children().each(function(i, d){
+                            existing_ele_width += $(d).width();
+                        });
+                        if(TOTAL_DIV_WIDTH - existing_ele_width >= INJECT_ELE_WIDTH)
+                            $(ele[0]).parent().append(inject_html);
+                    }
+                }
+            });
         }
     }
     // end of youku site inject
@@ -81,7 +116,7 @@
 
     function addWeiboTextBtn(node, vid){
         // add a play button beside origin play button
-        var inject_html = '<a name="iku_play" class="W_btn_c" href="' + _genInjectUrl(vid) + '"><span><em class="W_btn_icon"><i class="W_ico12 icon_cd_video"></i><i class="W_vline S_line1_c">|</i></em><em class="W_autocut S_link1">iku</em></span></a>';
+        var inject_html = '<a name="iku_play" class="W_btn_c" href="' + _genInjectUrl(vid) + '"><span><em class="W_btn_icon"><i class="W_ico12 icon_cd_video"></i><i class="W_vline S_line1_c">|</i></em><em class="W_autocut S_link1">iKu</em></span></a>';
         $(node).after(inject_html);
     }
     
@@ -105,7 +140,7 @@
     }
 
     function addWeiboSpecBtn(node, vid){
-        var inject_html = '<a class="W_btn_a" name="iku_play" href="' + _genInjectUrl(vid) + '"><span class="btn_26px">iku播放</span></a>';
+        var inject_html = '<a class="W_btn_a" name="iku_play" href="' + _genInjectUrl(vid) + '"><span class="btn_26px">iKu</span></a>';
         $(node).prepend(inject_html);
     }
 
